0a754811cbf6ebb8585235168431ba77ad687bb4
Triraypa <tristansabatier1804@gmail.com>
2024-03-17 19:39:04 +0100

    Test2

---
+++ app/Models/cart_M.php
@@ -0,0 +1,18 @@
+<?php
+include($WidgetsPathing . "connexion.php");
+
+foreach ($_SESSION['cart'] as $productId) {
+
+}
+
+//test 2
+
+$req = $dbh->prepare("SELECT * FROM articles");
+$req->setFetchMode(PDO::FETCH_ASSOC);
+$req->execute();
+
+while ($ligne = $req->fetch(PDO::FETCH_ASSOC)):
+    $article = $ligne["produit"];
+    $image = $ligne["img"];
+    $prix = $ligne["prix"];
+endwhile;
